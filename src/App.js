f4136cbad8f2821219b2c54194bd570e1b2c6c34
chxttp <thanakritpitiviroj@gmail.com>
2024-07-17 05:19:28 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from "react";
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import "./App.css";
+import car from "../src/images/car.png";
 
-function App() {
+const App = () => {
+  const [filteredCars, setFilteredCars] = useState([]);
+  const [selectedCar, setSelectedCar] = useState(null);
+  const [repairItems, setRepairItems] = useState([]);
+
+  const handleSearch = async (query) => {
+    if (query) {
+      const response = await axios.get(
+        `https://carregisteration.onrender.com/api/search?query=${query}`
+      );
+      setFilteredCars(response.data);
+    } else {
+      setFilteredCars([]);
+    }
+  };
+
+  const handleSelectCar = async (car) => {
+    setSelectedCar(car);
+    const response = await axios.get(`https://carregisteration.onrender.com/api/car/${car}`);
+    setRepairItems(response.data);
+    setFilteredCars([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={car} alt="Car" className="car-image" />
+      <h1>ค้นหารายการซ่อมรถ</h1>
+      <div className="search-container">
+        <SearchBar onSearch={handleSearch} />
+      </div>
+
+      <ul>
+        {filteredCars.map((car) => (
+          <li key={car} onClick={() => handleSelectCar(car)}>
+            {car}
+          </li>
+        ))}
+      </ul>
+      {selectedCar && (
+        <div className="selected-car">
+          <h2>รายการซ่อมของ {selectedCar}</h2>
+          <ul>
+            {repairItems.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,22 @@
+// src/components/SearchBar.js
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+    onSearch(e.target.value);
+  };
+
+  return (
+    <input
+      type="text"
+      value={query}
+      onChange={handleChange}
+      placeholder="ใส่เลขทะเบียนรถ"
+    />
+  );
+};
+
+export default SearchBar;
